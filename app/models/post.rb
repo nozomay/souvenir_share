3a589db6bb3b38affc213ec8a0521d80df323bdc
nozomay <n.yahagi1837@gmail.com>
2021-12-26 12:54:42 +0000

    [Add]レビュースコア(星)

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,9 @@
 class Post < ApplicationRecord
   belongs_to :user
+  
+  #星1以上〜５以下
+  validates :rate, numericality: {
+    less_than_or_equal_to: 5,
+    greater_than_or_equal_to: 1
+  }, presence: true
 end
