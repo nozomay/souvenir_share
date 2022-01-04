6fff188f997a68574df8d4b8879c4fb66ec48fa2
nozomay <n.yahagi1837@gmail.com>
2022-01-04 05:40:00 +0000

    [Fix/Add]Postコントローラ・gem追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,40 +1,57 @@
 class PostsController < ApplicationController
+  before_action :move_to_signed_in, except: [:index]
+
   def index
     @posts = Post.all
   end
-  
+
   def show
     @post = Post.find(params[:id])
   end
-  
+
   def new
     @post = Post.new
   end
-  
+
   def create
     @post = Post.new(post_params)
-    @post.save
-    redirect_to post_path(post.id)
+    @post.user_id = current_user.id
+    if @post.save
+      flash[:notice] = "投稿完了しました"
+      redirect_to post_path(@post)
+    else
+      render :new
+    end
   end
-  
+
   def edit
     @post = Post.find(params[:id])
   end
-  
+
   def update
     @post = Post.find(params[:id])
-    @post.update(post_params)
-    redirect_to post_path(post)
+    if @post.update(post_params)
+      flash[:notice] = "投稿内容を変更しました"
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
   end
-  
+
   def destroy
     @post = Post.find(params[:id])
     @post.destroy
     redirect_to posts_path
   end
-  
+
   private
   def post_params
     params.require(:post).permit(:title, :review, :rate, :address)
+  end
+
+  def move_to_signed_in
+    unless user_signed_in?
+      redirect_to  new_user_session_path
+    end
   end
 end
