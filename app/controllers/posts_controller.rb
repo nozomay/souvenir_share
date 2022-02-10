be482b74e583a10a80fcc6ab6c02a2ffb6b6e45f
nozomay <n.yahagi1837@gmail.com>
2022-02-10 13:13:02 +0000

    [Add]投稿削除後のメッセージを追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -59,6 +59,7 @@ class PostsController < ApplicationController
   def destroy
     @post = Post.find(params[:id])
     @post.destroy
+    flash[:notice] = "投稿を削除しました"
     redirect_to posts_path
   end
 
