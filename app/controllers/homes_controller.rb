10119a7ab2f87ba1a68a0a9a3d1f3b3e536a4e2c
nozomay <n.yahagi1837@gmail.com>
2022-06-07 11:26:13 +0000

    [Fix]Homeコントローラ

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,5 +1,7 @@
 class HomesController < ApplicationController
   def top
-    @random = Post.order("RANDOM()").limit(6)
+    # @random = Post.order("RANDOM()").limit(6)
+    @random = Post.find(Post.pluck(:id).shuffle[0..5])
+    pp @random
   end
 end
